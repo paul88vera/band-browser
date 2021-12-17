8292706e1a6df3968964e89be18d81ce96d2d344
loganscott678 <93100495+loganscott678@users.noreply.github.com>
2021-12-16 19:40:10 -0600

    renamed main page files

---
+++ assets/js/main-page.js
@@ -0,0 +1,5 @@
+var bandName = document.querySelector('.band-name');
+var bandImage = document.querySelector('.band-image');
+var eventInfo = document.querySelector('.event-info');
+var bandBio = document.querySelector('.band-bio');
+
