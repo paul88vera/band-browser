753599410cf91c200c7292198aeef57aaec55576
loganscott678 <93100495+loganscott678@users.noreply.github.com>
2021-12-16 20:46:22 -0600

    setup band info api call

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -2,4 +2,21 @@ var bandName = document.querySelector('.band-name');
 var bandImage = document.querySelector('.band-image');
 var eventInfo = document.querySelector('.event-info');
 var bandBio = document.querySelector('.band-bio');
+var bandInfoURL = 'theaudiodb.com/api/v1/json/2/search.php?s=coldplay'
 
+// var userInput = document.querySelector('.button');
+
+function getArtistInfo() {
+    fetch('https://theaudiodb.com/api/v1/json/2/search.php?s=coldplay') 
+    .then(function(response) {
+        if(response.ok) {
+            response.json()
+            .then(function(data) {
+                console.log(data)
+            })
+        }
+        else {console.log('error' + response.statusText)}
+    })
+};
+
+getArtistInfo();
